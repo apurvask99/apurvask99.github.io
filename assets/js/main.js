75362bae6de8014608468ca2a5e54701c0b84caf
Apurva Kulkarni <apurvask@uci.edu>
2020-09-07 11:49:43 -0700

    Updating main page

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,43 @@
 !(function($) {
   "use strict";
 
+  
+
+  // $(".typed").typed({
+	// 	strings: ["Software Engineer", "Backend Developer", "Data Engineer"],
+	// 	// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+	// 	stringsElement: null,
+	// 	// typing speed
+	// 	typeSpeed: 30,
+	// 	// time before typing starts
+	// 	startDelay: 1200,
+	// 	// backspacing speed
+	// 	backSpeed: 20,
+	// 	// time before backspacing
+	// 	backDelay: 500,
+	// 	// loop
+	// 	loop: true,
+	// 	// false = infinite
+	// 	loopCount: 5,
+	// 	// show cursor
+	// 	showCursor: false,
+	// 	// character for cursor
+	// 	cursorChar: "|",
+	// 	// attribute to type (null == text)
+	// 	attr: null,
+	// 	// either html or text
+	// 	contentType: 'html',
+	// 	// call when done callback function
+	// 	callback: function() {},
+	// 	// starting callback function before each string
+	// 	preStringTyped: function() {},
+	// 	//callback for every typed string
+	// 	onStringTyped: function() {},
+	// 	// callback for reset
+	// 	resetCallback: function() {}
+	// });
+
+
   // Preloader
   $(window).on('load', function() {
     if ($('#preloader').length) {
@@ -15,6 +52,20 @@
       });
     }
   });
+
+  //Hero typed
+  if ($('.typed').length) {
+    //var typed_strings = $(".typed").data('typed-items');
+    var typed_strings = "Software Engineer,Backend Developer,Data Engineer";
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
 
   // Smooth scroll for the navigation menu and links with .scrollto classes
   $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
